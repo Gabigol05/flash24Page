bf81a16e2ee59b563dec2d81162905779d81efe9
Gabriel <95046@sistemas.frc.utn.edu.ar>
2025-08-14 01:48:54 -0300

    Cambios 1:48

--- mysite/settingsProd.py
+++ mysite/settingsProd.py
@@ -1,41 +1,81 @@
-# mysite/settings/prod.py
-from .base import *
+# settingsBase.py
+
+from pathlib import Path
 import os
 
-# Producción: sin debug
-DEBUG = False
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Dominios permitidos (ajusta con tu usuario/dominio real)
-ALLOWED_HOSTS = [
-    'tuusuario.pythonanywhere.com',
-    'tudominio.com',
-    'www.tudominio.com',
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'first',
 ]
 
-# CSRF: orígenes de confianza (HTTPS)
-CSRF_TRUSTED_ORIGINS = [
-    'https://tuusuario.pythonanywhere.com',
-    'https://tudominio.com',
-    'https://www.tudominio.com',
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# PythonAnywhere está detrás de proxy: esto hace que Django reconozca HTTPS
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+ROOT_URLCONF = 'mysite.urls'
 
-# Cookies seguras si usás HTTPS
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
-# (Opcional, habilitalo cuando verifiques HTTPS ok)
-# SECURE_HSTS_SECONDS = 31536000
-# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-# SECURE_HSTS_PRELOAD = True
+WSGI_APPLICATION = 'mysite.wsgi.application'
 
-# Whitenoise para servir estáticos comprimidos desde la app
-# (recordá: pip install whitenoise)
-MIDDLEWARE = MIDDLEWARE.copy()
-MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
-# En producción EXIGIMOS que la SECRET_KEY venga del entorno
-SECRET_KEY = os.environ['SECRET_KEY']
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_TZ = True
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
